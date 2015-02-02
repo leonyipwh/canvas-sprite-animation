1ef6adf536d57206b1c79cba0ee45036072120fb
Leon Yip <leon.yip@tbwa.com>
2015-02-02 15:41:15 +0800

    canvas width and height

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,91 @@
-var hashData;
+//config
 
-function changePage(e)
-{
-  window.location.hash = '#'+ e;
+var fps = 24;
+// var imgPath1 = '../images/char9.png';
+// var imgPath1 = '../images/fire.png';
+var imgPath1 = './images/ice.png';
+
+var frameHeight;
+var frameWidth;
+
+var canvas = null;
+var img = null;
+var ctx = null;
+var imageReady = false;
+var frame = 0;
+
+function onload() {
+  canvas = document.getElementById('gameCanvas');
+  ctx = canvas.getContext('2d');
+  img = new Image();
+  img.src = imgPath1;
+  img.onload = loaded();
+
+  frameHeight = canvas.height;
+  frameWidth = canvas.width;
 }
 
-function getHash()
-{
-  hashData = window.location.hash.substring(1);
-
-	if (hashData!==''){
-    changePage(hashData);
-	}
+function loaded() {
+  imageReady = true;
+  setTimeout( update, 1000 / 60 );
 }
 
-$(window).on('hashchange', function()
-{
-  hashData = window.location.hash.substring(1);
+function redraw() {
 
-	$('.page').hide();
-	$('#p'+hashData).show();
-});
+  ctx.clearRect(0, 0, frameWidth, frameHeight);
+  ctx.fillStyle = 'rgba(0, 0, 200, 0)';
+  ctx.fillRect(0, 0, frameWidth, frameHeight);
+  // if (imageReady) {
+      // ctx.drawImage(img, frame*96, 0, 96, 54,canvas.width/2 - 48, canvas.height/2 - 48, 96, 54);
+      // ctx.drawImage(img, frame*96, 0, 96, 54,canvas.width/2 - 200, canvas.height/2 - 48, 70, 38);
+      // ctx.drawImage(img, frame*96, 0, 96, 54,canvas.width/2 + 100, canvas.height/2 - 48, 60, 30);
+      //context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
+      ctx.drawImage(img, frame*96, 0, 96, 54,0, 0, frameWidth, frameHeight);
+  // }
+}
+
+function update(){
+  redraw();
+  frame++;
+  if (frame >= 6)
+  {
+    frame = 0;
+  }
+  setTimeout( update, 1000 / fps );
+}
+
+
+function textUpdate()
+{
+  var text = document.getElementById('inputBox').value;
+
+  console.log(text);
+
+  $('#svgContainer').html('<svg id="svg" height="86px" width="300px"><text id="copyText" x="10" y="50%" fill="white" contenteditable="true">'+text+'<animate attributeName="x" from="0%" to="100%" begin="0s" dur="4s" repeatCount="indefinite"></animate></text></svg>');
+
+}
+
+
+// var hashData;
+//
+// function changePage(e)
+// {
+//   window.location.hash = '#'+ e;
+// }
+//
+// function getHash()
+// {
+//   hashData = window.location.hash.substring(1);
+//
+// 	if (hashData!==''){
+//     changePage(hashData);
+// 	}
+// }
+//
+// $(window).on('hashchange', function()
+// {
+//   hashData = window.location.hash.substring(1);
+//
+// 	$('.page').hide();
+// 	$('#p'+hashData).show();
+// });

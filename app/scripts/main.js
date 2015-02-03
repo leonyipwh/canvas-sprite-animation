e35ca4ca1ff50290fffdddc09b63d2b8392df8ce
Leon Yip <leon.yip@tbwa.com>
2015-02-03 18:07:04 +0800

    canvas border

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,7 +15,8 @@ var imageReady = false;
 var frame = 0;
 var hashData;
 
-function onload() {
+function canvasAni()
+{
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
   img = new Image();
@@ -66,10 +67,11 @@ function textUpdate()
 
 }
 
-
 function changePage(e)
 {
   window.location.hash = '#'+ e;
+  // $('.page').hide();
+  // $('#'+e).show();
 }
 
 function getHash()
@@ -86,151 +88,5 @@ $(window).on('hashchange', function()
   hashData = window.location.hash.substring(1);
 
 	$('.page').hide();
-	$('#p'+hashData).show();
+	$('#'+hashData).show();
 });
-
-
-//Photo
-var width;
-var canvasHeight;
-
-function getSize()
-{
-  width = $(window).width();
-  // console.log("window width= "+width);
-  canvasWidth = $(window).width();
-  height = $(window).height();
-
-  canvasHeight = width * 590/945;
-}
-
-window.onload = function() {
-  var fileInput = document.getElementById('fileUpload');
-  fileInput.onchange = function() {
-    var file = fileInput.files[0];
-    // MegaPixImage constructor accepts File/Blob object.
-    var mpImg = new MegaPixImage(file);
-
-    // Render resized image into image element using quality option.
-    // Quality option is valid when rendering into image element.
-    // var resImg = document.getElementById('resultImage');
-    // mpImg.render(resImg, { maxWidth: 300, maxHeight: 300, quality: 0.5 });
-    //
-    // // Render resized image into canvas element.
-    // var resCanvas1 = document.getElementById('resultCanvas1');
-    // mpImg.render(resCanvas1, { maxWidth: 300, maxHeight: 300 });
-
-    // Render resized image into canvas element, rotating orientation = 6 (90 deg rotate right)
-    // Types of orientation is defined in EXIF specification.
-    // To detect orientation of JPEG file in JS, you can use exif.js from https://github.com/jseidelin/exif-js
-    var resCanvas2 = document.getElementById('resultCanvas2');
-    mpImg.render(resCanvas2, {maxWidth: 1000, maxHeight: 1000, quality: 1, orientation: 6 });
-
-    setTimeout("draw()",1000);
-  };
-};
-
-//kinetic.js
-var stage = new Kinetic.Stage
-({
-  container: 'canvasContainer',
-  width: width,
-  height: canvasHeight
-});
-
-var layer = new Kinetic.Layer();
-
-$( '#fileUpload' ).change(function()
-{
-  layer.destroy();
-});
-
-function draw()
-{
-  degree = 0;
-
-  var imageObj = new Image();
-
-  imageObj.onload = function()
-  {
-    oriImgHeight = imageObj.height;
-    oriImgWidth = imageObj.width;
-
-    //zoom ratio
-    var zw = width/oriImgWidth;
-    var zh = canvasHeight/oriImgHeight;
-    console.log("zw= " + zw);
-    console.log("zh= " + zh);
-
-    currentZ = Math.max(zw, zh);
-
-    minZ = currentZ;
-
-    ratioImgWidth = minZ*oriImgWidth;
-    ratioImgHeight = minZ*oriImgHeight;
-    editedHeight = ratioImgHeight;
-
-    ratio = oriImgWidth/oriImgHeight;
-
-    wideWidth = (canvasHeight*ratio);
-
-    minX = -((ratioImgWidth-width)/2 - width/2);
-    maxX = ((ratioImgWidth-width)/2 + width/2);
-    minY = -((ratioImgHeight-canvasHeight)/2 - canvasHeight/2);
-    maxY = ((ratioImgHeight-canvasHeight)/2 + canvasHeight/2);
-
-    oriX = ((stage.getWidth()/2)-(ratioImgWidth/2)+ratioImgWidth/2);
-    oriY = ((stage.getHeight()/2)-(ratioImgHeight/2)+ratioImgHeight/2);
-
-    myImage = new Kinetic.Image
-    ({
-      image: imageObj,
-      width: ratioImgWidth,
-      height: editedHeight,
-      x: oriX,
-      y: oriY,
-      offsetX:ratioImgWidth/2,
-      offsetY:ratioImgHeight/2,
-      draggable: true,
-      dragBoundFunc: function (pos)
-      {
-        var X = pos.x;
-        var Y = pos.y;
-        if (X < minX) {
-            X = minX;
-        }
-        if (X > maxX) {
-            X = maxX;
-        }
-        if (Y < minY) {
-            Y = minY;
-        }
-        if (Y > maxY) {
-            Y = maxY;
-        }
-        return ({
-            x: X,
-            y: Y
-        });
-      }
-    });
-
-    layer.add(myImage);
-    stage.add(layer);
-  };
-
-  /*var f = document.getElementById('fileUpload').files[0];
-  var name = f.name;
-  var url = window.URL || window.webkitURL;
-  var src = url.createObjectURL(f);
-
-
-  imageObj.src = src;*/
-
-  imageObj.src = document.getElementById("resultCanvas2").toDataURL("image/png",1);
-
-  stage.draw();
-  // url.revokeObjectURL(src);
-
-  // $("#fileUpload").replaceWith($("#fileUpload").val('').clone(true));
-}

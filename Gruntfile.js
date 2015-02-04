89965b62521cc9bae2864b149be1b424333ae898
Leon Yip <leon.yip@tbwa.com>
2015-02-04 16:51:20 +0800

    music added

--- Gruntfile.js
+++ Gruntfile.js
@@ -343,7 +343,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/{,*/}*.*'
+            'fonts/{,*/}*.*',
+            'music/{,*/}*.*'
           ]
         }, {
           expand: true,
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@
 
 var fps = 24;
 var imgPath1 = './images/ice.png';
+var imgPath2 = './images/fire.png';
 
 var frameHeight;
 var frameWidth;
@@ -14,15 +15,49 @@ var frame = 0;
 var hashData;
 
 //customise item
-var theme =0;
+var customData=[];
+var videoNum;
+
+var theme;
 var msgValue;
+var msgText;
+var video;
+
+
+
+function preview()
+{
+  var previewGifPath;
+
+  if (customData.theme===1)
+  {
+    previewGifPath = 'images/dancer.gif';
+  }else
+  {
+    previewGifPath = 'images/band.gif';
+  }
+
+  $('#previewGif').attr('src',previewGifPath);
+
+  canvasAni();
+
+  previewSvg();
+}
 
 function canvasAni()
 {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
   img = new Image();
-  img.src = imgPath1;
+
+    if (customData.videoNum===1)
+    {
+      img.src = imgPath1;
+    }else
+    {
+      img.src = imgPath2;
+    }
+
   img.onload = loaded();
 
   frameHeight = canvas.height;
@@ -58,6 +93,11 @@ function update(){
   setTimeout( update, 1000 / fps );
 }
 
+function previewSvg()
+{
+  $('#svgContainer').html('<svg id="svg" height="86px" width="300px"><text id="copyText" x="10" y="70%" fill="white" contenteditable="true">'+customData.msgText+'<animate attributeName="x" from="0%" to="100%" begin="0s" dur="4s" repeatCount="indefinite"></animate></text></svg>');
+}
+
 
 function textUpdate()
 {
@@ -67,13 +107,6 @@ function textUpdate()
 
   $('#svgContainer').html('<svg id="svg" height="86px" width="300px"><text id="copyText" x="10" y="70%" fill="white" contenteditable="true">'+text+'<animate attributeName="x" from="0%" to="100%" begin="0s" dur="4s" repeatCount="indefinite"></animate></text></svg>');
 
-}
-
-function saveText()
-{
-  var text = document.getElementById('inputBox').value;
-
-  msgValue = text;
 }
 
 function changePage(e)
@@ -117,9 +150,85 @@ videoManager.prototype.play = function(x) {
     this.gif.src = 'images/band.gif';
   }
 
-  theme=x;
+  this.theme = x;
 };
 
-videoManager.prototype.stop = function() {
+videoManager.prototype.save = function() {
   this.audio.pause();
+  theme=this.theme;
 };
+
+
+//middleManager
+
+var targetCanvas = null;
+var canvasImg = null;
+var targetCtx = null;
+
+function canvasManager(canvasId, source)
+{
+  targetCanvas = document.getElementById(canvasId);
+  targetCtx = targetCanvas.getContext('2d');
+  canvasImg = new Image();
+  canvasImg.onload = loadCanvas();
+
+  if (source===1) {
+    canvasImg.src = imgPath1;
+  }
+  else
+  {
+    canvasImg.src = imgPath2;
+  }
+
+  video = source;
+
+  frameHeight = targetCanvas.height;
+  frameWidth = targetCanvas.width;
+}
+
+function loadCanvas() {
+  imageReady = true;
+  setTimeout( updateCanvas, 1000 / 60 );
+}
+
+function redrawCanvas() {
+
+  targetCtx.clearRect(0, 0, frameWidth, frameHeight);
+  targetCtx.fillStyle = 'rgba(0, 0, 200, 0)';
+  targetCtx.fillRect(0, 0, frameWidth, frameHeight);
+  // if (imageReady) {
+      // ctx.drawImage(img, frame*96, 0, 96, 54,canvas.width/2 - 48, canvas.height/2 - 48, 96, 54);
+      // ctx.drawImage(img, frame*96, 0, 96, 54,canvas.width/2 - 200, canvas.height/2 - 48, 70, 38);
+      // ctx.drawImage(img, frame*96, 0, 96, 54,canvas.width/2 + 100, canvas.height/2 - 48, 60, 30);
+      //context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
+      targetCtx.drawImage(canvasImg, frame*96, 0, 96, 54,0, 0, frameWidth, frameHeight);
+  // }
+}
+
+function updateCanvas(){
+  redrawCanvas();
+  frame++;
+  if (frame >= 6)
+  {
+    frame = 0;
+  }
+  setTimeout( updateCanvas, 1000 / fps );
+}
+
+function saveMsg()
+{
+  var text = document.getElementById('msgInputBox').value;
+  videoNum=video;
+}
+
+function updateMsgText()
+{
+  var text = document.getElementById('msgInputBox').value;
+
+  console.log(text);
+
+  msgText= text;
+
+  $('#msgSvgContainer').html('<svg id="msgSvg" height="86px" width="300px"><text id="copyText" x="10" y="70%" fill="white" contenteditable="true">'+text+'<animate attributeName="x" from="0%" to="100%" begin="0s" dur="4s" repeatCount="indefinite"></animate></text></svg>');
+
+}

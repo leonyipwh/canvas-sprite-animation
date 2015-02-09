cba7e9f8308cb3ac4e838e3bb0e8e0d25a59c40c
Leon Yip <leon.yip@tbwa.com>
2015-02-09 20:01:49 +0800

    music function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,11 +4,27 @@ var video;
 var customData;
 var msgText='hihi';
 
+var audioPlayer = document.createElement('audio');
+audioPlayer.setAttribute('src', 'music/1.mp3');
+
 function changePage(e)
 {
   window.location.hash = '#'+ e;
   $('.page').hide();
   $('#'+e).show();
+
+  audioPlayer.pause();
+
+  if (e==='text')
+  {
+    audioPlayer.play();
+  }
+  else if(e==='preview_2d' || e==='preview_4d')
+  {
+    // var audioPlayer = document.createElement('audio');
+    audioPlayer.setAttribute('src', 'music/'+ customData.video +'.mp3');
+    audioPlayer.play();
+  }
 }
 
 function previewTheme(e)
@@ -53,14 +69,20 @@ $(window).on('hashchange', function() {
 
 function previewVideo(e)
 {
+
   if (e===1) {
     $('#textImg').find('img').attr('src', 'images/redBall.gif');
+      audioPlayer.setAttribute('src', 'music/1.mp3');
   }else
   {
     $('#textImg').find('img').attr('src', 'images/yellowBall.gif');
+    audioPlayer.setAttribute('src', 'music/2.mp3');
   }
 
   video=e;
+
+  audioPlayer.play();
+
   console.log('selected theme: '+ e);
 }
 
@@ -74,23 +96,27 @@ function build()
   if (customData.theme === 1)
   {
     $('.frontImage').attr('src','images/man.gif');
-  }else{
+  }
+  else
+  {
     $('.frontImage').attr('src','images/women.gif');
   }
 
-    if (customData.video === 1)
-    {
-      $('.video').attr('src','images/redBall.gif');
-    }else{
-      $('.video').attr('src','images/yellowBall.gif');
-    }
+  if (customData.video === 1)
+  {
+    $('.video').attr('src','images/redBall.gif');
+  }
+  else
+  {
+    $('.video').attr('src','images/yellowBall.gif');
+  }
 
   $('.previewText').html(customData.msgText);
 }
 
 function updateText()
 {
-  var text = $("#textInput").val();
-  $("#customText").html(text);
+  var text = $('#textInput').val();
+  $('#customText').html(text);
   msgText = text;
 }
